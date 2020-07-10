e65377d1448a773038d5244946fd0f851bd7de8a
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-09 20:38:00 -0700

    Creating variable that writes Markdown files

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const fs = require('fs')
 
 const api = require('./utils/api.js')
 const generateMarkdown = require('./utils/generateMarkdown.js')
+
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName + '.md', data, error => error ? console.error(error) : console.log(`${fileName + '.md'} generated!`))
+  }
